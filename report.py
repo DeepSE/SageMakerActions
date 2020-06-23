fae79b6ac0d8daf6cde4ca687f5e173fc464897b
Sung Kim <hunkim@gmail.com>
2020-06-23 11:11:26 +0900

    Added comment test

--- report.py
+++ report.py
@@ -7,6 +7,7 @@ import pandas as pd
 import base64
 from io import BytesIO, StringIO
 from sagemaker.analytics import TrainingJobAnalytics
+from comment import Comment
 
 def read_json(filepath):
     """
@@ -27,47 +28,37 @@ def update_leaderboard(score, scoreText="Score",
         leaderboard_indicator = ".leaderboard.csv\n", 
         lb_branch_name = "_lb_",
         ascending=False):
-    # search a pull request that triggered this action
-    gh = Github(os.getenv('GITHUB_TOKEN'))
     
+    comment = Comment()
 
-    # https://developer.github.com/webhooks/event-payloads/#pull_request
-    event = read_json(os.getenv('GITHUB_EVENT_PATH'))
-    repo = gh.get_repo(event['repository']['full_name'])
-    pr = repo.get_pull(int(event['number']))
+    pr_number = comment.pr.number 
+    pr_sender = comment.pr.user.username 
 
-    # Testing
-    repos_name = os.getenv('GITHUB_REPOSITORY')
-    pr_number = os.getenv('PR_NUMBER')
-    pr.create_issue_comment(repos_name + ":" + pr_number)
-
-
-    pr_number = pr.number
-    pr_sender = event['sender']['login']
     entry = "#" + str(pr_number) + " by " + str(pr_sender)
  
     leaderboard_content = None
-    pr1 = repo.get_pull(1)
-    for comment in pr1.get_issue_comments():
-        if comment.body.startswith(leaderboard_indicator):
-            if len(comment.body)>len(leaderboard_indicator):
-                leaderboard_content = comment.body[len(leaderboard_indicator):]
-            comment.delete()
-    
-    if not leaderboard_content:
-        leaderboard_content = scoreText + ", Entity"
-    
+
+    comment_pr1 = Comment(pr_number=1)
+    values = comment_pr1.get_comment(leaderboard_indicator)
+
+    if comment_pr1 is None or len(values) is 0:
+                leaderboard_content = scoreText + ", Entity"
+    else:
+        leaderboard_content = values[0]
+
+    # Delete the comment
+    comment_pr1.del_comment(leaderboard_indicator)
+
     df = pd.read_csv(StringIO(leaderboard_content))
     df.loc[len(df.index)] =  [score, entry] 
     df = df.sort_values(by=df.columns[0], ascending=ascending)
     new_leaderbord_content = df.to_csv(index=False)
 
-    pr1.create_issue_comment(leaderboard_indicator + new_leaderbord_content)
+    comment_pr1.add_comment(leaderboard_indicator + new_leaderbord_content)
 
     # Add new leaderboard results as a comment
     leaderboard_md = "## New Leaderboard\n" + df.to_markdown()
-    leaderboard_md_base64 = base64.b64encode(leaderboard_md.encode("utf-8")).decode("utf-8")
-    print(f"::set-output name=LEADERBOARD_MD::{leaderboard_md_base64}")
+    comment.add_comment(leaderboard_md)
 
 def report(lb_config):
     ########################################################################
@@ -77,11 +68,12 @@ def report(lb_config):
     score_name = lb_config['score_name']
     score_metric = lb_config['score_metric']
 
+    comment = Comment()
+
     training_job_name = estimator.latest_training_job.name
     trained_model_location = "model_data=" + estimator.model_data
     print(trained_model_location)
-    trained_model_location_base64 = base64.b64encode(trained_model_location.encode("utf-8")).decode("utf-8")
-    print(f"::set-output name=MODEL_LOCATION::{trained_model_location_base64}")
+    comment.add_comment(trained_model_location)
     
     # Get metric values
     metric_names = [ metric['Name'] for metric in estimator.metric_definitions ] 
@@ -89,9 +81,7 @@ def report(lb_config):
     metrics_dataframe = TrainingJobAnalytics(training_job_name=training_job_name, metric_names=metric_names).dataframe()
     result_md = "## Results\n"+ metrics_dataframe.to_markdown()
     print(result_md)
-
-    result_md_base64 = base64.b64encode(result_md.encode("utf-8")).decode("utf-8")
-    print(f"::set-output name=RESULT_MD::{result_md_base64}")
+    comment.add_comment(result_md)
 
     # Update leaderboard. Make sure the key name is right
     # Use any name if you don't want to use the leaderboard
@@ -113,13 +103,6 @@ def report(lb_config):
 
 
 if __name__ == '__main__':
-    result_md = "Testing!"
-    print(f"::set-output name=RESULT_MD::{result_md}")
-
-    leaderboard_md = "## New Leaderboard\n"
-    print(f"::set-output name=LEADERBOARD_MD::{leaderboard_md}")
-
-    trained_model_location="test.gz"
-    print(f"::set-output name=MODEL_LOCATION::{trained_model_location}")
+    pass
 
 
