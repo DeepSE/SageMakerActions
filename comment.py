7b0498851ebba530f77764d04751d4d71b323f4b
Sung Kim <hunkim@gmail.com>
2020-06-23 09:56:07 +0900

    Added comment test

--- comment.py
+++ comment.py
@@ -25,10 +25,35 @@ def add_comment(message):
     repo = gh.get_repo(repos_name)
     pr = repo.get_pull(pr_number)
     
-    pr.create_issue_comment(repos_name + ":" + pr_number + message)
+    pr.create_issue_comment(message)
 
+def get_comment_content(key):
+    # search a pull request that triggered this action
+    gh = Github(os.getenv('GITHUB_TOKEN'))
+    repos_name = os.getenv('GITHUB_REPOSITORY')
+    pr_number = os.getenv('PR_NUMBER')
+
+    if pr_number is None or repos_name is None:
+        return None
+    
+    repo = gh.get_repo(repos_name)
+    pr = repo.get_pull(pr_number)
+
+    values = []
+    for comment in pr.get_issue_comments():
+        if comment.body.startswith(key):
+            if len(comment.body)>len(key):
+                value = comment.body[len(key):]
+                values.append(value)
+    
+    return values
 
 if __name__ == '__main__':
     add_comment("hi there")
+    values = get_comment_content('model_data=')
+    for value in values:
+        add_comment('found models' + value)
+
+
 
 
