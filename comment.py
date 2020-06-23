2692ce1a6e0ab0d1196036e6ac16afb3417fbc7e
Sung Kim <hunkim@gmail.com>
2020-06-23 09:18:24 +0900

    Added comment test

---
+++ comment.py
@@ -0,0 +1,27 @@
+# Original code from 
+# https://github.com/harupy/comment-on-pr/blob/master/entrypoint.py
+from github import Github
+import os
+import json
+import pandas as pd
+import base64
+from io import BytesIO, StringIO
+from sagemaker.analytics import TrainingJobAnalytics
+
+def add_comment(message):
+    # search a pull request that triggered this action
+    gh = Github(os.getenv('GITHUB_TOKEN'))
+    repos_name = os.getenv('GITHUB_REPOSITORY')
+    #pr_number = os.getenv('PR_NUMBER')
+    pr_number = os.getenv('GITHUB_EVENT_NUMBER')
+
+    repo = gh.get_repo(repos_name)
+    pr = repo.get_pull(pr_number)
+    
+    pr.create_issue_comment(repos_name + ":" + pr_number + message)
+
+
+if __name__ == '__main__':
+    add_comment("hi there")
+
+
