92debe829f18cfd00941529815a591e37e9f7b6d
Sung Kim <hunkim@gmail.com>
2020-06-23 19:31:21 +0900

    pr check

--- sm_deply.py
+++ sm_deply.py
@@ -2,17 +2,30 @@ import sagemaker
 import boto3
 from sagemaker.pytorch import PyTorch
 from sagemaker.pytorch import PyTorchModel
+from comment import Comment
 
 sagemaker_session = sagemaker.Session(boto3.session.Session())
 
 # Put the right role and input data
 role = "arn:aws:iam::294038372338:role/hunkimSagemaker"
 
-model = PyTorchModel(model_data=model_data,
+comment = Comment()
+values = comment.get_comment('model_data=')
+if values is None or len(values) == 0:
+    comment.add_comment('Deploy Fail: no model data. Did you train?')
+    exit(-1)
+
+
+model = PyTorchModel(model_data=values[-1],
                      role=role,
                      framework_version='1.5.0',
-                     entry_point='que_gen.py',
-                     source_dir='code',
-                     predictor_cls=JSONPredictor)
+                     entry_point='mnist.py',
+                     source_dir='code')
 
-predictor = model.deploy(initial_instance_count=1, instance_type='ml.m4.xlarge')
+
+comment.add_comment('Deploying ...')
+try:
+    predictor = model.deploy(initial_instance_count=1, instance_type='ml.m4.xlarge')
+    comment.add_comment('end_point=' + predictor.endpoint)
+except Exception as e:
+    comment.add_comment('Deploy Fail:' + str(e))
